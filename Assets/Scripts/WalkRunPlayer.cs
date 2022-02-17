9ba0ae921570d2eb44856fdbbc47b023d9cfb5fd
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-17 16:00:58 +0100

    Begonnen aan een third person player

--- Assets/Scripts/WalkRunPlayer.cs
+++ Assets/Scripts/WalkRunPlayer.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class WalkRunPlayer : MonoBehaviour
 {
+    [Header("Movement aanpassingen")]
+    [SerializeField] float speed = 10f;
+    [SerializeField] float cameraSensX = 1f;
+    [SerializeField] Camera playerEyes;
+
+
     float horizontal;
     float vertical;
-    public float speed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +22,22 @@ public class WalkRunPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Walk();
+        Run();
+
+        transform.Rotate(0, mouseX * cameraSensX * Time.deltaTime, 0);
+    }
+
+    void Walk()
+    {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
         transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
-
-        Run();
     }
 
     void Run()
