6bdfd7037d9048de771f60c164c5e2f110203743
MikeHuijgen <mikeh162001@hotmail.com>
2022-02-17 19:41:11 +0100

    Third person camera gefixed

--- Assets/Scripts/WalkRunPlayer.cs
+++ Assets/Scripts/WalkRunPlayer.cs
@@ -6,8 +6,7 @@ public class WalkRunPlayer : MonoBehaviour
 {
     [Header("Movement aanpassingen")]
     [SerializeField] float speed = 10f;
-    [SerializeField] float cameraSensX = 1f;
-    [SerializeField] Camera playerEyes;
+    [SerializeField] float playerSens = 700f;
 
 
     float horizontal;
@@ -16,28 +15,29 @@ public class WalkRunPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
         Walk();
         Run();
-
-        transform.Rotate(0, mouseX * cameraSensX * Time.deltaTime, 0);
     }
 
     void Walk()
     {
+        //Move Player
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
         transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
+
+        //Move player with camera
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseX * playerSens * Time.deltaTime, 0);
     }
 
     void Run()
@@ -51,4 +51,5 @@ public class WalkRunPlayer : MonoBehaviour
             speed = 10f;
         }
     }
+
 }
