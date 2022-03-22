455468debfe68dc2cc0751e71453630e41eb8749
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2022-03-22 11:10:49 +0100

    Pickup niet gelukt. Samen de code met op zijn laptop verder gemaakt.

--- Assets/Scripts/TakeAndDrop.cs
+++ Assets/Scripts/TakeAndDrop.cs
@@ -7,82 +7,57 @@ public class TakeAndDrop : MonoBehaviour
     [Header("Layers")]
     [SerializeField] private LayerMask PickupLayer;
 
-    [Header("Camera's")]
-    [SerializeField] private Camera PlayerCamera;
-
     [Header("Floats")]
-    [SerializeField] private float ThrowingForce;
-    [SerializeField] private float PickupRange;
+    [SerializeField] private float ThrowingForce = 8f;
+    [SerializeField] private float PickupRange = 6f;
 
     [Header("Transforms")]
     [SerializeField] private Transform Hand;
 
-    private Rigidbody CurrentObjectRigidbody; //Move it around and have the object in hand.
-    private Collider CurrentObjectCollider;
+    [Header("GameObjects")]
+    [SerializeField] private GameObject item;
 
+    [Header("Bools")]
+    [SerializeField] private bool Carring;
 
-    private void Update()
+    Rigidbody rb;
+
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Carring == false)
         {
-            Ray Pickupray = new Ray(PlayerCamera.transform.position, PlayerCamera.transform.forward);
-
-            if (Physics.Raycast(Pickupray, out RaycastHit hitInfo, PickupRange, PickupLayer))
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                if (CurrentObjectRigidbody)
-                {
-                    //Calculated in the physics loop
-                    CurrentObjectRigidbody.isKinematic = false;
-                    CurrentObjectCollider.enabled = true;
-
-                    //Replace it with a new object
-                    CurrentObjectRigidbody = hitInfo.rigidbody;
-                    CurrentObjectCollider = hitInfo.collider;
-
-                    //Then the object becomes Kinematic
-                    CurrentObjectRigidbody.isKinematic = true;
-                    CurrentObjectCollider.enabled = false;
-                }
-                else // if we don't have an Item in the hand
-                {
-                    CurrentObjectRigidbody = hitInfo.rigidbody;
-                    CurrentObjectCollider = hitInfo.collider;
-
-                    CurrentObjectRigidbody.isKinematic = true;
-                    CurrentObjectCollider.enabled = false;
-                }
-
-                return; //if you do hit a object.
-            }
-
-            if (CurrentObjectRigidbody)//Reset your object
-            {
-                CurrentObjectRigidbody.isKinematic = false;
-                CurrentObjectCollider.enabled = true;
-
-                CurrentObjectRigidbody = null;
-                CurrentObjectCollider = null;
+                pickup();
+                rb = item.GetComponent<Rigidbody>();
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.Q))
+        else if (Carring == true)
         {
-            if (CurrentObjectRigidbody)
+            if (Input.GetKeyDown(KeyCode.Q))
             {
-                CurrentObjectRigidbody.isKinematic = false;
-                CurrentObjectCollider.enabled = true;
-
-                CurrentObjectRigidbody.AddForce(PlayerCamera.transform.forward * ThrowingForce, ForceMode.Impulse); // You can throw the object with a force behind it.
-
-                CurrentObjectRigidbody = null;
-                CurrentObjectCollider = null;
+                drop();
+              
             }
-        }
-
-        if (CurrentObjectRigidbody)
-        {
-            CurrentObjectRigidbody.position = Hand.position; //If the object is in the hand where is it.
-            CurrentObjectRigidbody.rotation = Hand.rotation; //If the object is in the hand how is it rotated.
         }
     }
+    void pickup()
+    {
+        Carring = true; 
+        Debug.Log("Heb het in mijn hand");
+        item.transform.parent = Hand.transform;
+        item.transform.position = Hand.transform.position;
+        item.transform.rotation = Hand.transform.rotation;
+        rb.useGravity = false; 
+    }
+    void drop()
+    {
+        Carring = false; 
+        Debug.Log("Laat het vallen");
+        item.transform.DetachChildren();
+        item.transform.position = Hand.transform.position;     
+        rb.AddForce(0, 0, ThrowingForce, ForceMode.Impulse);
+        rb.useGravity = true; 
+    }
 }
+
