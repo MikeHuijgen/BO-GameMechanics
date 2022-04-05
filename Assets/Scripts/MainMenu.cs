6167185862bdac0316af022204bee48aa6140c69
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2022-04-05 13:32:11 +0200

    begonnen aan de main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Zorgt ervoor als je op playbutton klikt ga je naar de volgende scene.
+                                                                              //Wat je hebt ingesteld in de build. 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Dit zorgt ervoor dat je de game helemaal afsluit als je de game hebt gebuild. 
+    }
+}
+
